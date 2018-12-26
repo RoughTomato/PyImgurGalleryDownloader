5be562557232f144f7a3275e585bdf79d4b5e1ee
RoughTomato <roughtomato@LorenzoTheLappy.localdomain>
2018-12-26 13:40:19 +1300

    functionality for adding and saving your api key

--- imgurDownloader.py
+++ imgurDownloader.py
@@ -3,8 +3,7 @@ from argparse import ArgumentParser
 import requests
 import json
 import urllib2
-
-authKey = ''
+import os
 
 def downloadFromURL(url, name):
     request = urllib2.Request(url)
@@ -26,17 +25,32 @@ def mimeToExtension(mime):
     return mimetype[mime]()
 
 parser = ArgumentParser()
-parser.add_argument("-u", "--url", dest="url",
-                            help="provide url for the gallery", metavar="URL")
+parser.add_argument("-g", "--gallery", dest="gallery",
+                            help="provide gallery hash e.g. \"x3z0vpj\"", metavar="GALLERY")
+parser.add_argument("-a", "--auth", dest="authkey",
+                            help="provide authorisation API key", metavar="AUTH")
+parser.add_argument("-s", "--save", dest="savekey", 
+                            help="store authorisation API key", metavar="SAVE")
 
 args = parser.parse_args()
-if args.url is None:
+if args.gallery is None:
+    parser.print_help()
+elif args.authkey is None and not os.path.isfile('./authkey.txt'):
     parser.print_help()
 else:
-    url = "https://api.imgur.com/3/gallery/" + args.url
+    if args.authkey is None and os.path.isfile('./authkey.txt'):
+        with open('./authkey.txt','r') as authfile:
+            authkey = authfile.read()
+    else:
+        authkey = args.authkey
+    if not args.savekey is None:
+        authfile = open('./authkey.txt', 'w')
+        authfile.write(authkey)
+
+    url = "https://api.imgur.com/3/gallery/" + args.gallery
     payload = {}
     headers = {
-              'Authorization': 'Client-ID ' + authKey
+              'Authorization': 'Client-ID ' + authkey
     }
     response = requests.request('GET', url, headers = headers, data = payload, allow_redirects=0)
 
